3f719a65f1972df72277e70b147dbe17746f58a3
Florian Wilkens <github@wilkens-florian.de>
2018-09-23 21:17:17 +0200

    Create muster-plugin.php

---
+++ muster-plugin/muster-plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Wilk-Muster Plugin
+Plugin URI: https://wilkens-florian.de/development/wordpress/
+Description: Plugin Muster (<a target="_blank" href="https://wilkens-florian.de/development/wordpress/">Informationen</a>)
+Version: 1.0 dev
+Author: Florian Wilkens
+Author URI: https://wilkens-florian.de
+Update Server: https://wilkens-florian.de/development/wordpress/
+Min WP Version: 4.9.6
+Max WP Version: 4.9.6
+License: FREE
+License URI: https://wilkens-florian.de/development/wordpress/
+Text Domain: wilkens-florian
+*/
+/* PluginCode beginnt hier */
+?> 
+<?php
+
+//some Code right here
+echo "Das ist das Muster Plugin"; //Fügt einen Text am Anfang der Seite ein
+
+?>
+<?php
+/* PluginCode endet hier */
+?>
